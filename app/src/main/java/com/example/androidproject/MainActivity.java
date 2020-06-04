fd6744483ab705a9644b6dfee7384f1a9a5c4ddb
MAHAMAT ALAMINE <mahamatmasse83@gmail.com>
2020-06-04 08:43:29 +0200

    finalisation de la dernière interface

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -45,15 +45,15 @@ public class MainActivity extends AppCompatActivity {
                 .setLenient()
                 .create();
         List<Player> listPlayerPreferences = getDataPreferences();
-//        if(listPlayerPreferences != null){
-//            playerDetailsList = listPlayerPreferences;
-//
-//            setData(playerDetailsList);
-//        } else {
-//
-//            apiCall();
-//        }
-        apiCall();
+        if(listPlayerPreferences != null){
+            playerDetailsList = listPlayerPreferences;
+
+            setData(playerDetailsList);
+        } else {
+
+            apiCall();
+        }
+
 
     }
 
